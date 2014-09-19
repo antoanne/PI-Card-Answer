e703adc94b56a05cf5e31f37d0cc73d3eb5b1614
Antoanne Pontes <antoanne@gmail.com>
2014-09-19 09:09:14 -0300

    Rotacionar 0.5º as imagens que não conseguiram ser processadas.

---
+++ RotateImage.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import sys
+
+arq = sys.argv[1]
+
+img = cv2.imread(arq,0)
+rows,cols = img.shape
+
+M = cv2.getRotationMatrix2D((cols/2,rows/2),0.5,1)
+dst = cv2.warpAffine(img,M,(cols,rows))
+
+cv2.imwrite(arq, dst)
+'''
+cv2.imshow('result',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
